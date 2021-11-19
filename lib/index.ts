262b843afad804f78b73f3f02fe02ebe49ec5b2d
Jordan Kueh <jkueh@users.noreply.github.com>
2021-11-19 18:58:54 +1100

    0.1.4 - Adding default exports to the root library, and the RunnerAMI library

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,12 @@
 import * as cdk from '@aws-cdk/core';
-import { RunnerAMI } from './runner-ami';
+import RunnerAMI from './runner-ami';
 
 export interface GithubRunnersProps {
   asgName: string // The name of the ASG to create
   tags?: { [key: string]: string },
 }
 
-export class GithubRunners extends cdk.Construct {
+export default class GithubRunners extends cdk.Construct {
 
   constructor(scope: cdk.Construct, id: string, props: GithubRunnersProps) {
     super(scope, id);
